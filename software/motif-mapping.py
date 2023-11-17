2219dc86a24192d820f8844e38a4f5c08af5ee6c
kmartinez834 <karinamartinez@gwu.edu>
2023-11-17 15:39:29 -0500

    added motif-mapping.py to generate a mapping file with motif name and ids for Glycan Highlight widget

---
+++ software/motif-mapping.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+'''
+This file will generate a mapping file with motif name and ids for Glycan Highlight widget
+
+Example:
+[
+    {
+        "id" : "GGM.000001",
+        "name": "Type 2 LN"
+    },
+    {
+        "id" : "GGM.000005",
+        "name": "Type 1 LN"
+    },
+]'''
+
+
+import os
+import glob
+import csv
+import json
+
+def main():
+
+    mapping_list = []
+
+    motif_file = "/data/projects/glygen/downloads/glytoucan/current/export/allmotifs.tsv"
+
+    with open(motif_file, "r") as in_file:
+        infile = csv.reader(in_file, delimiter="\t")
+
+        for row in infile:
+            motif_id,prop,motif_name = row[0],row[1],row[2]
+            if prop == 'PreferredName':
+                mapping_list.append(dict(id=motif_id,name=motif_name))
+        
+    json_data = json.dumps(mapping_list, indent=4)
+
+    # Writing to sample.json
+    out_file = '/data/projects/glygen/generated/misc/motif_mapping.json'
+    with open(out_file, "w") as outfile:
+        outfile.write(json_data)
+
+    print (f"Saved {out_file}")
+
+if __name__ == '__main__':
+    main()
