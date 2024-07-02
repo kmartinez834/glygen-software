4e8e4e3f51001463236d108fb1e212672dd47100
kmartinez834 <karinamartinez@gwu.edu>
2024-07-02 13:49:31 -0400

    simplify parser, add csv file output

--- software/carbbank-mapping.py
+++ software/carbbank-mapping.py
@@ -23,41 +23,43 @@ import os
 import glob
 import csv
 import json
+import re
+import csv
 
 def main():
 
     entry_dict = {}
 
     src_file = "/data/projects/glygen/downloads/carbbank/current/carbbank.txt"
-    headers = ("AG:","AM:","AN:","AU:","BA:","BS:","CC:","CT:","DA:","DB:","FC:","MT:","NC:","NT:","PA:","PM:","SB:","SC:","SD:","SI:","ST:","structure:","TI:","TN:","VR:")
 
     with open(src_file, "r", encoding = 'latin-1') as in_file:
         infile = in_file.readlines()
         
         record_num = 0
         prev_line = ""
+        new_record = False
         for line in infile:
             line = line.strip("\n")
             if line.startswith("; start of record"):
                 record_num += 1
                 entry_dict[record_num] = {}
+                new_record = True
                 continue
-            elif line.startswith(("; ", "----------------","================end of record")):
+            elif line.startswith("----------------"):
+                new_record = False
                 continue
-            elif line.startswith(headers):
-                prev_line = line
-            else:
-                prev_line += line
-
-            key = prev_line.split(":")[0]
-            if key in [i.split(":")[0] for i in headers]:
+            elif new_record == True and line.startswith(";")==False:
+                if re.match(r"^[A-Z][A-Z]:", line):
+                    prev_line = line
+                else:
+                    prev_line += line
+                key = prev_line.split(":")[0]
                 value = prev_line.split(f"{key}: ")[-1]
                 entry_dict[record_num][key] = value
-                       
-    for key,value in entry_dict.items():
-        if "structure" in value:
-            value.pop("structure")
-
+            else:
+                continue
+    
+    # write to json file
     json_data = json.dumps(entry_dict, indent=4)
 
     # Writing to sample.json
@@ -66,6 +68,33 @@ def main():
         outfile.write(json_data)
 
     print (f"Saved {out_file}")
+
+    # write to csv file
+    headers = []
+    for value in entry_dict.values():
+        for header in value.keys():
+            if header not in headers:
+                headers.append(header)
+
+    out_file = '/home/karinamartinez/carbbank.csv'
+    with open(out_file, "w") as outfile:
+        csv_writer = csv.writer(outfile,quoting=csv.QUOTE_ALL)
+        count = 0
+        for record in entry_dict.values():
+            if count == 0:
+                csv_writer.writerow(headers)
+                count += 1
+
+            for header in headers:
+                if header not in record:
+                    record[header] = ""
+
+            sorted_dict = {i: record[i] for i in headers}
+            csv_writer.writerow(sorted_dict.values())
+
+    print (f"Saved {out_file}")
+            
+    
 
 if __name__ == '__main__':
     main()
